21301fcd61cec38514f7868d882398c22499d5fc
Radmin <bekazw@gmail.com>
2018-01-30 16:22:09 +0300

    Redux initialize

--- shop01/src/index.js
+++ shop01/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import initStore from './reducers/init';
+
+let store0 = createStore ( initStore ); 
+
+console.log (store0.getState());
+
+ReactDOM.render(
+    <Provider store = { store0 } >
+        <App />
+    </Provider>    
+    , document.getElementById('root'));
+
---
+++ shop01/src/reducers/init.js
@@ -0,0 +1,26 @@
+const state0 = {
+    currentBasketId : -1,
+    productsToBuy : [] ,
+    productsCount : []
+}
+
+const initStore = (state = state0, action) => {
+    switch ( action.type ) {
+        case "ADD_PRODUCT" :
+            return {
+                ...state,
+                    currentBasketId : action.basketId,
+                    productsToBuy : [ ...state.productsToBuy, action.prodId ],
+                    productsCount : [ ...state.productsCount, action.prodCount ]
+                    }
+        case "CREATE_BASKET" :
+            return {
+                ...state,
+                currentBasketId : action.basketId
+            }
+        default :
+            return state        
+    }    
+}
+
+export default initStore;
