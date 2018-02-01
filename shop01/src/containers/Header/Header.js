26c4a7ee5eeec3b5f69cdd88e88d650d9f80a58d
Radmin <bekazw@gmail.com>
2018-02-01 09:41:43 +0300

    темп

--- shop01/src/containers/Header/Header.js
+++ shop01/src/containers/Header/Header.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import './Header.css';
+
+const Search = () => {
+    return (
+        <div id = 'header_search'>
+            <input  id ="search_text" type = "text" placeholder = "search text" />
+            <button id = "search_button"> Search! </button>
+        </div>
+    )
+}
+
+const Login = () => {
+    return (
+        <div id = 'header_login'>
+            <button id = "search_login"> LogIn </button>
+            <button id = "search_reg" > RegMe </button>
+        </div>
+    )
+}
 
 const Header = () => {
     return (
-        <div>
-            header
+        <div id = 'header_main'>
+            <div id = 'header_logo'>LOGO</div>
+            <Search />
+            <Login />
         </div>
     )
 }
--- shop01/src/containers/Menu/Menu.js
+++ shop01/src/containers/Menu/Menu.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+const MenuElement = (props) => {
+    return (
+        <button className = 'menu_element' > {props.menuName} </button> 
+    )
+}
+
 const Menu = () => {
     return (
-        <div>
-                Menu
+        <div id = "menu_main">
+            <MenuElement menuName = "HOME" />
+            <MenuElement menuName = "ABOUT" />
+            <MenuElement menuName = "CONTACTS" />
         </div>
     )
 }
