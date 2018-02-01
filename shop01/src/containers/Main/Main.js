479f4a715faa6d9d757d578eee982338ad82c92a
Radmin <bekazw@gmail.com>
2018-02-01 13:45:08 +0300

    ProductCard

--- shop01/src/containers/Main/Main.js
+++ shop01/src/containers/Main/Main.js
@@ -9,58 +9,11 @@ const ProductCard = (props) => {
     )
 }
 
-const Main = () => {
+const Main = (props) => {
+    let arr = [1,2,3];
     return (
         <div id = "main"> 
-            <ProductCard  productId = {1}/>
-            <ProductCard productId = {2} />
-            <ProductCard productId = {3} />
-            <ProductCard productId = {4} />
-            <ProductCard productId = {5} />
-            <ProductCard productId = {6} />
-            <ProductCard productId = {7}/>
-            <ProductCard productId = {8}/>
-            <ProductCard productId = {9}/>
-            <ProductCard productId = {10} />
-            <ProductCard productId = {11}/>
-            <ProductCard productId = {12}/>
-            <ProductCard productId = {13}/>
-            <ProductCard productId = {14}/>
-            <ProductCard productId = {15}/>
-            <ProductCard productId = {16}/>
-            <ProductCard productId = {17}/>
-            <ProductCard productId = {18}/>
-            <ProductCard productId = {19}/>
-            <ProductCard productId = {20}/>
-            <ProductCard productId = {21}/>
-            <ProductCard productId = {22}/>
-            <ProductCard productId = {23}/>
-            <ProductCard productId = {24}/>
-            <ProductCard productId = {25}/>
-            <ProductCard productId = {26}/>
-            <ProductCard productId = {27}/>
-            <ProductCard productId = {28}/>
-            <ProductCard productId = {29}/>
-            <ProductCard productId = {30}/>
-            <ProductCard productId = {31}/>
-            <ProductCard productId = {32}/>
-            <ProductCard productId = {33}/>
-            <ProductCard productId = {34}/>
-            <ProductCard productId = {35}/>
-            <ProductCard productId = {36}/>
-            <ProductCard productId = {37}/>
-            <ProductCard productId = {38}/>
-            <ProductCard productId = {39}/>
-            <ProductCard productId = {40}/>
-            <ProductCard productId = {41}/>
-            <ProductCard productId = {42}/>
-            <ProductCard productId = {43}/>
-            <ProductCard productId = {44}/>
-            <ProductCard productId = {45}/>
-            <ProductCard productId = {46}/>
-            <ProductCard productId = {47}/>
-            <ProductCard productId = {48}/>
-
+            {arr.map(item => <ProductCard productId = {item} />)}
         </div>
     )
 }
